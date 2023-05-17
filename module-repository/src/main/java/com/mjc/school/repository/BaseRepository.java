dfb1e84a6f9cf36e68a5358428e92f6dcf334d46
Renata Yermak <renatkahturo@gmail.com>
2023-05-17 19:49:01 +0300

    .

--- module-repository/src/main/java/com/mjc/school/repository/BaseRepository.java
+++ module-repository/src/main/java/com/mjc/school/repository/BaseRepository.java
@@ -2,6 +2,7 @@ package com.mjc.school.repository;
 
 import com.mjc.school.repository.model.BaseEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -23,47 +24,3 @@ public interface BaseRepository<T extends BaseEntity<K> , K> {
 
     boolean existById(K id);
 }
-//@RequiredArgsConstructor
-//public abstract class BaseRepository<T extends Serializable, E extends BaseEntity<T>> implements Repository<T, E> {
-//
-//    private final Class<E> clazz;
-//    protected final EntityManager entityManager;
-//
-//    @Override
-//    public Optional<E> readById(T id) {
-//        return Optional.ofNullable(entityManager.find(clazz, id));
-//    }
-//
-//    @Override
-//    public List<E> readAll() {
-//        var criteria = entityManager.getCriteriaBuilder().createQuery(clazz);
-//        criteria.from(clazz);
-//        return entityManager.createQuery(criteria)
-//                .getResultList();
-//    }
-//
-//    @Override
-//    public E create(E entity) {
-//        entityManager.persist(entity);
-//        return entity;
-//    }
-//
-//    @Override
-//    public E update(E entity) {
-//        entityManager.merge(entity);
-//        return entity;
-//    }
-//
-//    @Override
-//    public boolean deleteById(T id) {
-//        entityManager.find(clazz, id);
-//        entityManager.remove(entityManager.find(clazz, id));
-//        entityManager.flush();
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean existById(T id){
-//        return readById(id).isPresent();
-//    }
-//}
